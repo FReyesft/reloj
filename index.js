275c116acc03a69a8bbe04fba53acfcc01008b10
Fernando Reyez Pinto <f.reyeft@gmail.com>
2023-11-14 16:09:42 -0500

    add 0 when the date element time is minor than 10

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const hours = document.getElementById('date-hours')
-const minutes = document.getElementById('date-minutes')
-const seconds = document.getElementById('date-seconds')
+let hoursClock = document.getElementById('date-hours')
+let minutesClock = document.getElementById('date-minutes')
+let secondsClock = document.getElementById('date-seconds')
+
+
+function UpdateTimeForSecond() {
+	setInterval(() => {
+		const date = new Date
+		let hours = date.getHours()
+		let minutes = date.getMinutes();
+		let seconds = date.getSeconds();
+		console.log(seconds)
+		if (seconds < 10) {
+			secondsClock.textContent = '0' + seconds
+		} else {
+			secondsClock.textContent = seconds
+		}
+		if (minutes < 10) {
+			minutesClock.textContent = '0' + minutes
+		} else {
+			minutesClock.textContent = minutes
+		}
+		if (hours < 10) {
+			hoursClock.textContent = '0' + hours
+		} else {
+			hoursClock.textContent = hours
+		}
+
+
+
+
+
+
+
+
+	}, 1000)
+}
+
+UpdateTimeForSecond();
