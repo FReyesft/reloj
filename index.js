f1a83895de1c2aae064f9c653c2c48e095c0835e
Fernando Reyez Pinto <f.reyeft@gmail.com>
2023-11-17 16:55:33 -0500

    change styles and update textContent for innerText

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ let secondsClock = document.getElementById('date-seconds')
 let dateDay = document.getElementById('date')
 const timesClock = document.querySelectorAll('.times-clock')
 
+
+
 function UpdateTimeForSecond() {
 	setInterval(() => {
 		const date = new Date
-		let hours = date.getHours();
+		let hours = date.getHours() % 12;
 		let minutes = date.getMinutes();
 		let seconds = date.getSeconds();
 		dateDay.textContent = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
 		console.log(seconds)
 		if (seconds < 10) {
-			secondsClock.textContent = '0' + seconds
+			secondsClock.innerText = '0' + seconds
 		} else {
-			secondsClock.textContent = seconds
+			secondsClock.innerText = seconds;
 		}
 		if (minutes < 10) {
-			minutesClock.textContent = '0' + minutes
+			minutesClock.innerText = '0' + minutes
 		} else {
-			minutesClock.textContent = minutes
+			minutesClock.innerText = minutes
 		}
 		if (hours < 10) {
-			hoursClock.textContent = '0' + hours
+			hoursClock.innerText = '0' + hours
 		} else {
-			hoursClock.textContent = hours
+			hoursClock.innerText = hours
 		}
 		timesClock.forEach((item) => {
 			item.classList.toggle('active');
